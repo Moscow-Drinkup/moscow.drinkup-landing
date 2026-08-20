f231a93283fcc02d53e456fa7f17fa5ba1b88cb4
SayPoj <saypoj@users.noreply.github.com>
2026-08-21 02:28:02 +0300

    cleanup: удалён устаревший generated-event

---
+++ node_modules/@gravity-ui/page-constructor/node_modules/@gravity-ui/dynamic-forms/build/cjs/lib/unstable/kit/controls/Slider/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Slider = void 0;
+var Slider_1 = require("./Slider");
+Object.defineProperty(exports, "Slider", { enumerable: true, get: function () { return Slider_1.Slider; } });
--- src/generated-event.ts
+++
@@ -1,7 +0,0 @@
-// Авто-генерируемый файл: ближайшее событие из API Networkly (см. scripts/fetch-event.mjs)
-export const nextEvent = {
-  "name": "Moscow DrinkUp #11",
-  "id": 6699,
-  "url": "https://networkly.app/event/6699",
-  "start": null
-} as const;
